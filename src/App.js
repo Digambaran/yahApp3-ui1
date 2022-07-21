b0323b863a2d31787f6d4e19c57d8ad9bd3507b9
Digambaran <arjunsreekumar.1@gmail.com>
2022-07-21 18:25:26 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Yahapp3-ui1 from './yahApp3-ui1'
+export default function App() {
+  return (
+    <div>
+      <Yahapp3-ui1/>
+    </div>
+  )
+}
---
+++ src/yahApp3-ui1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Yahapp3-ui1 = () => {
+  return (
+   <p>Hello from yahApp3-ui1 </p>
+  )
+}
+export default Yahapp3-ui1
